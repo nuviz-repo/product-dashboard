c5db58de1a3e8415e2523a5d3fd11fa76b9fd9a2
João Carlos da Rosa <joaocarlosdarosa@MacBook-Pro-de-Joao.local>
2025-01-31 11:05:34 -0300

    feat: adding https api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,7 @@
 // services/mongoService.ts
 // const API_URL = 'http://localhost:8000';
-const API_URL = window.location.protocol === 'https:' 
-  ? `wss://ec2-18-189-143-77.us-east-2.compute.amazonaws.com:8000`
-  : `ws://ec2-18-189-143-77.us-east-2.compute.amazonaws.com:8000`;
-  
+const API_URL = "https://api.nuviz.com.br";
+
 interface DateRangeRequest {
   start_date: string;
   end_date: string;
