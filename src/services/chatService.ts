85c93a87e828ab240771d5e2b24f04ec04b85993
João Carlos da Rosa <joaocarlosdarosa@MacBook-Pro-de-Joao.local>
2025-02-11 22:00:29 -0300

    fix: chat problem

--- src/services/chatService.ts
+++ src/services/chatService.ts
@@ -2,9 +2,9 @@
 import { DailyMetric } from "@/types/api";
 import { ChatResponse, Message, TimelineData, TimelineDataForChat } from "@/types/chat";
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8001/api';
+// const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8001/api';
 
-// const API_URL = import.meta.env.VITE_API_URL || 'https://chat-api.nuviz.com.br/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://chat-api.nuviz.com.br/api';
 
 export const chatService = {
     initialize: async (message: string, data: TimelineDataForChat, dailyMetric: DailyMetric[]): Promise<string> => {
